bef123af143e54672c952b513b3b015b3ad53f04
Akash-Mishra <mishraakash18@gmail.com>
2016-09-07 00:43:18 +0530

    app complete

--- app/src/main/java/com/example/batcomputer/recordandplay/MainActivity.java
+++ app/src/main/java/com/example/batcomputer/recordandplay/MainActivity.java
@@ -1,13 +1,70 @@
 package com.example.batcomputer.recordandplay;
 
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+
+public class MainActivity extends Activity {
+
+    private MediaRecorder myAudioRecorder;
+    private String outputFile = null;
+    private Button start, stop, play;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        start = (Button)findViewById(R.id.button1);
+        stop = (Button)findViewById(R.id.button2);
+        play = (Button)findViewById(R.id.button3);
+
+        stop.setEnabled(false);
+        play.setEnabled(false);
+        outputFile = Environment.getExternalStorageDirectory().getAbsolutePath() + "/myrecording.3gp";
+
+        myAudioRecorder = new MediaRecorder();
+        myAudioRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        myAudioRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+        myAudioRecorder.setOutputFile(outputFile);
+    }
+
+    public void start(View view) {
+        try {
+            myAudioRecorder.prepare();
+            myAudioRecorder.start();
+        } catch (IllegalStateException e){
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        start.setEnabled(false);
+        stop.setEnabled(true);
+        Toast.makeText(getApplicationContext(), "Recording started", Toast.LENGTH_LONG).show();
+    }
+
+    public void stop(View view){
+        myAudioRecorder.stop();
+        myAudioRecorder.release();
+        myAudioRecorder = null;
+        stop.setEnabled(false);
+        play.setEnabled(true);
+        Toast.makeText(getApplicationContext(), "Audio Recorded Successfully", Toast.LENGTH_LONG).show();
+    }
+
+    public void play(View view) throws IllegalArgumentException, SecurityException, IllegalStateException, IOException{
+        MediaPlayer m = new MediaPlayer();
+        m.setDataSource(outputFile);
+        m.prepare();
+        m.start();
+        Toast.makeText(getApplicationContext(), "Playing Audio", Toast.LENGTH_LONG).show();
     }
 }
